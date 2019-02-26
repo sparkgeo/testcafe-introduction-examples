ef78524d350f450962673b86e132c17b30ce463a
Brian Bancroft <hello@brianbancroft.ca>
2019-02-26 13:44:17 -0500

    ✅  Test marker properties

--- tests/1-map-render.js
+++ tests/1-map-render.js
@@ -1,8 +1,25 @@
 import { Selector } from 'testcafe';
 
 fixture `Leaflet Map - Testing Render`
-    .page `http://localhost:3001`;
+    .page `http://localhost:3001/sandbox`;
 
-test('My first test', async t => {
-    // Test code
+test('The suite loads', async t => {
+  // Test code
 });
+
+const addMarker = Selector('#addMarker');
+
+test('The marker is visible', async t => {
+  await t
+  .click(addMarker)
+  .expect(Selector('.leaflet-marker-icon').exists)
+  .ok('The Leaflet marker must populate')
+})
+
+test('The marker image pulls from the correct source', async t => {
+  await t
+    .click(addMarker)
+    .expect(Selector('.leaflet-marker-icon')
+    .withAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png'))
+    .ok('The marker must come from the proper source')
+})
